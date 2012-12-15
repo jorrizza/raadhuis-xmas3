124b112993988f390ab75a3695d91fd783f590a4
Joris van Rooij <jorrizza@jrrzz.net>
2012-12-15 20:03:06 +0100

    Added poller and made Firefox suck less.

--- public/js/xmas3.js
+++ public/js/xmas3.js
@@ -1,10 +1,24 @@
-var color = '#ff0000';
+function color_poller () {
+  $.ajax(
+    {
+      url: '/get-color'
+    }
+  ).done(
+    function (color) {
+      $(".led").css({stroke: '#'+color});
+    }
+  );
+
+  setTimeout(color_poller, 500);
+}
 
 $(function () {
+    color_poller();
+
     $('#color-container button').click(
       function () {
-        color = rgb2hex($(this).css('background-color'));
-        $(".led").css({stroke: color});
+        var color = rgb2hex($(this).css('background-color'));
+
         $.ajax(
           {
             url: '/set-color',
@@ -15,11 +29,13 @@ $(function () {
     );
   }
 );
- 
+
 function rgb2hex(rgb) {
   rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+
   function hex(x) {
     return ("0" + parseInt(x).toString(16)).slice(-2);
   }
+
   return "#" +hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 }
