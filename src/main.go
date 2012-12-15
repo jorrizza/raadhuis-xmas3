d57c57f915775b4d3654d02a4e93f14b5055384e
Joris van Rooij <jorrizza@jrrzz.net>
2012-12-15 19:35:11 +0100

    Added get-color API call. Also, made some little queue changes.

--- src/main.go
+++ src/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"strconv"
+	"sync"
 	"time"
 )
 
 var remote string
 var q chan []byte
+var lastColor []byte
+var colorMutex sync.Mutex
 
 func parseColor(c string) []byte {
 	if len(c) > 0 {
@@ -25,7 +30,7 @@ func parseColor(c string) []byte {
 		}
 	}
 
-	return []byte{0xFF, 0x00, 0x00}
+	return lastColor
 }
 
 func setColor(color []byte) {
@@ -40,10 +45,41 @@ func setColor(color []byte) {
 	conn.Write(color)
 	conn.Write([]byte{0x00})
 	conn.Close()
+
+	colorMutex.Lock()
+	copy(lastColor, color)
+	colorMutex.Unlock()
+}
+
+func getColor(w http.ResponseWriter, r *http.Request) {
+	colorMutex.Lock()
+	fmt.Fprintf(w, "%02X%02X%02X", lastColor[0], lastColor[1], lastColor[2])
+	colorMutex.Unlock()
 }
 
 func queueColor(w http.ResponseWriter, r *http.Request) {
 	q <- parseColor(r.FormValue("value"))
+}
+
+func initializeColor() {
+	conn, err := net.Dial("tcp", remote)
+
+	if err != nil {
+		log.Print(err.Error())
+		return
+	}
+
+	conn.Write([]byte{0xDE, 0xAD, 0xBA, 0xBE, 0x00})
+	time.Sleep(100 * time.Millisecond)
+	_, err = bufio.NewReader(conn).Read(lastColor)
+
+	if err != nil {
+		log.Print(err.Error())
+		return
+	}
+
+	fmt.Printf("initialized with color #%02X%02X%02X\n",
+		lastColor[0], lastColor[1], lastColor[2])
 }
 
 func queue() {
@@ -57,18 +93,22 @@ func queue() {
 }
 
 func main() {
-	q = make(chan []byte)
+	q = make(chan []byte, 120)
+	lastColor = make([]byte, 3)
 
 	port := flag.String("p", "localhost:4321", "socket to listen on")
 	public := flag.String("d", "public", "directory of public files")
 	flag.StringVar(&remote, "c", "localhost:9000", "host to connect to")
 
+	go queue()
+
 	flag.Parse()
+
+	initializeColor()
 
 	http.Handle("/", http.FileServer(http.Dir(*public)))
 	http.HandleFunc("/set-color", queueColor)
-
-	go queue()
+	http.HandleFunc("/get-color", getColor)
 
 	log.Fatal(http.ListenAndServe(*port, nil))
 }
