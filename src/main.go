a955caa4801fc9849df2226554ab0644a85cb691
Joris van Rooij <jorrizza@jrrzz.net>
2012-12-15 15:31:14 +0100

    Added queue and zero padding to keep arduino happy.

--- src/main.go
+++ src/main.go
@@ -7,9 +7,11 @@ import (
 	"net"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 var remote string
+var q chan []byte
 
 func parseColor(c string) []byte {
 	if len(c) > 0 {
@@ -27,20 +29,36 @@ func parseColor(c string) []byte {
 	return []byte{0xFF, 0x00, 0x00}
 }
 
-func setColor(w http.ResponseWriter, r *http.Request) {
+func setColor(color []byte) {
 	conn, err := net.Dial("tcp", remote)
 
 	if err != nil {
 		log.Print(err.Error())
-		return
+	} else {
+		conn.Write([]byte{0x73})
+		conn.Write(color)
+		conn.Write([]byte{0x00})
+		conn.Close()
 	}
+}
 
-	conn.Write([]byte{0x73})
-	conn.Write(parseColor(r.FormValue("value")))
-	conn.Close()
+func queueColor(w http.ResponseWriter, r *http.Request) {
+	q <- parseColor(r.FormValue("value"))
+}
+
+func queue() {
+	for {
+		newColor := <-q
+
+		time.Sleep(500 * time.Millisecond)
+
+		setColor(newColor)
+	}
 }
 
 func main() {
+	q = make(chan []byte)
+
 	port := flag.Int("p", 4321, "port to listen on")
 	public := flag.String("d", "public", "directory of public files")
 	flag.StringVar(&remote, "c", "localhost:9000", "host to connect to")
@@ -48,7 +66,9 @@ func main() {
 	flag.Parse()
 
 	http.Handle("/", http.FileServer(http.Dir(*public)))
-	http.HandleFunc("/set-color", setColor)
+	http.HandleFunc("/set-color", queueColor)
+
+	go queue()
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
